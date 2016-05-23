9b34878764448db425e84f8854de95d47830d149
phaneesh <phaneesh.n@gmail.com>
2016-05-23 09:46:23 +0530

    Upgrade to 2.4 & add Kafka

--- hdp-basic/download-warmup-deps.sh
+++ hdp-basic/download-warmup-deps.sh
@@ -3,6 +3,6 @@
 #The exit of download_deps needs to be trapped because the Docker build exits
 function download_deps {
   yum install -y yum-plugin-downloadonly
-  yum install -y --downloadonly hadoop_*-yarn* hadoop_*-hdfs* hadoop_*-hcatalog* hadoop_*-metastore* snappy snappy-devel ambari-log4j httpd python-rrdtool-1.4.5 hive_* mysql mysql-server tez_* fping
+  yum install -y --downloadonly hadoop_*-yarn* hadoop_*-hdfs* hadoop_*-hcatalog* hadoop_*-metastore* snappy snappy-devel ambari-log4j httpd python-rrdtool-1.4.5 hive_* mysql mysql-server tez_* fping kafka
 }
 trap download_deps EXIT
